1f12584a73b9f4f87ffa5b8c682fa381c3a4523d
Shumer-1 <borisov1908vlad@mail.ru>
2025-12-24 19:03:48 +0300

    add task04.sol

---
+++ lab6/Task_03.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Task_03 {
+    function generateFactorial(uint256 n) external pure returns (uint256) {
+        uint256 result = 1;
+        uint256 i = 1;
+
+        while (i <= n) {
+            result *= i;
+            i++;
+        }
+
+        return result;
+    }
+}
