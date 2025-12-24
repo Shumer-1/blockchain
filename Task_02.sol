bad2a1a607d802c64f3f45fe368af0315e760cbd
Shumer-1 <borisov1908vlad@mail.ru>
2025-12-24 18:18:51 +0300

    add task02.sol

--- Task_02.sol
+++ Task_02.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
 contract Task_02 {
-   uint[] public dynamicArray; // Динамический массив
-   uint[5] public fixedArray;   // Фиксированный массив из 5 элементов
+    uint[] public dynamicArray;
+    uint[5] public fixedArray;
 
+    function addToDynamicArray(uint _value) external {
+        dynamicArray.push(_value);
+    }
 
-   function addToDynamicArray(uint _value) external {
-       dynamicArray.push(_value); // Добавление элемента в динамический массив
-   }
+    function sumArray() public view returns (uint sum) {
+        for (uint i = 0; i < dynamicArray.length; i++) {
+            sum += dynamicArray[i];
+        }
+        return sum;
+    }
 }
