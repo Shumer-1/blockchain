e2abd1b398ecc03fa2c9107e9506ead1b8e982d0
Shumer-1 <borisov1908vlad@mail.ru>
2025-12-24 19:06:54 +0300

    add task05.sol

--- lab6/Task_04.sol
+++ lab6/Task_04.sol
@@ -1,17 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Task_04 {
-    function generateFibo(uint256 n) external pure returns (uint256) {
-        uint256 a = 0;
-        uint256 b = 1;
+contract Task_05 {
+    int256[] public celsiusTemps;
 
-        do {
-            uint256 next = a + b;
-            a = b;
-            b = next;
-        } while (a <= n);
+    function setCelsiusTemps(int256[] calldata temps) external {
+        delete celsiusTemps;
+        for (uint256 i = 0; i < temps.length; i++) {
+            celsiusTemps.push(temps[i]);
+        }
+    }
 
-        return a;
+    function addCelsiusTemp(int256 temp) external {
+        celsiusTemps.push(temp);
+    }
+
+    function getAllTempsInFahrenheit() external view returns (int256[] memory) {
+        int256[] memory result = new int256[](celsiusTemps.length);
+
+        for (uint256 i = 0; i < celsiusTemps.length; i++) {
+            result[i] = _toFahrenheit(celsiusTemps[i]);
+        }
+
+        return result;
+    }
+
+    function convertArrayToFahrenheit(int256[] calldata temps) external pure returns (int256[] memory) {
+        int256[] memory result = new int256[](temps.length);
+
+        for (uint256 i = 0; i < temps.length; i++) {
+            result[i] = _toFahrenheit(temps[i]);
+        }
+
+        return result;
+    }
+
+    function _toFahrenheit(int256 c) private pure returns (int256) {
+        return (c * 9) / 5 + 32;
     }
 }
